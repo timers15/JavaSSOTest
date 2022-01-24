6395fdbbb30bef86031ea2b19e6c47c5238b926d
Tim Dobrowolsky <tdobrowolsky@ccbcmd.edu>
2022-01-24 13:39:26 -0500

    qgain

--- src/main/java/com/ccbc/JavaWebAppSSO/JavaWebAppSsoApplication.java
+++ src/main/java/com/ccbc/JavaWebAppSSO/JavaWebAppSsoApplication.java
@@ -2,10 +2,9 @@ package com.ccbc.JavaWebAppSSO;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JavaWebAppSsoApplication extends SpringBootServletInitializer {
+public class JavaWebAppSsoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaWebAppSsoApplication.class, args);
